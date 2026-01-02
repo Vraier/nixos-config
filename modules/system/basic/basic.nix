dc2627ef5c4872ac584d2290a201b73fa7762ab4
Vraier <jpvdheydt@googlemail.com>
2026-01-02 14:51:36 +0100

    System rebuild: 2026-01-02 14:51

--- modules/system/basic/basic.nix
+++ modules/system/basic/basic.nix
@@ -1,6 +1,17 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs.thunar.enable = true;
 
   services.tailscale.enable = true;
   services.tailscale.useRoutingFeatures = "client";
+
+  services.tailscale.permitCertUid = "jp";
+  networking.firewall = {
+    checkReversePath = "loose";
+    trustedInterfaces = ["tailscale0"];
+    allowedUDPPorts = [config.services.tailscale.port];
+  };
 }
