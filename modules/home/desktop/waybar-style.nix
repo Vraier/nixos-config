cea7ec1327f6302ac4a44403499ba813b0fdcb30
Vraier <jpvdheydt@googlemail.com>
2026-01-02 15:30:28 +0100

    System rebuild: 2026-01-02 15:30

--- modules/home/desktop/waybar-style.nix
+++ modules/home/desktop/waybar-style.nix
@@ -105,11 +105,6 @@
        /* Fix moving mute button */
        border-radius: 10px;
        transition: all 0.3s ease;
-       min-width: 600px;
-       margin-left: 0px;
-    margin-right: 0px;
-    padding-left: 0px;
-    padding-right: 0px;
    }
 
    #pulseaudio.bluetoothed {
