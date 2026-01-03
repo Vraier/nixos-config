9bec159fe9782b9b5218d641bc871a1c5bcff510
Vraier <jpvdheydt@googlemail.com>
2026-01-03 16:13:19 +0100

    System rebuild: 2026-01-03 16:13

--- modules/system/basic/basic.nix
+++ modules/system/basic/basic.nix
@@ -3,5 +3,17 @@
   config,
   ...
 }: {
-  programs.thunar.enable = true;
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs.xfce; [
+      thunar-archive-plugin
+      thunar-volman
+    ];
+  };
+  services.gvfs.enable = true;
+  services.tumbler.enable = true;
+
+  environment.systemPackages = [
+    pkgs.gvfs
+  ];
 }
